fedfaeee0609bcd0e537c9896e804cb7867333b0
Spaghetti-12 <elliot.sicheri@mail.utoronto.ca>
2023-12-08 12:28:02 -0500

    updated url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,1 +1,1 @@
-export const baseURL = "https://cors-everywhere.herokuapp.com/django-env.eba-89phmv2c.us-west-2.elasticbeanstalk.com/";
+export const baseURL = "http://django-env.eba-89phmv2c.us-west-2.elasticbeanstalk.com/";
